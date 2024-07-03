71d50ed1d17ab3baa33fa15e68984fe332e653df
Александра Быковская <Aibykovs@mail.ru>
2024-07-03 15:59:29 +0700

    Добавила разметку для главной страницы

--- app/src/main/java/com/example/pizza_shift_2024/MainActivity.kt
+++ app/src/main/java/com/example/pizza_shift_2024/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.pizza_shift_2024
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pizza_shift_2024.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private var _binding: ActivityMainBinding? = null
+    private val binding: ActivityMainBinding
+        get() = _binding ?: throw IllegalStateException("Binding in Main Activity must not be null")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
